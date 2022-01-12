5d1c764bc1376bbfad5fd9c813c7e2ae22b7e17f
Matheus Galvão Correia <matheus.correia@avenuecode.com>
2022-01-12 17:43:17 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <Button>Testing</Button>
+  </div>
+</template>
+
+<script>
+import { Button } from 'ant-design-vue'
+
+export default {
+  name: 'App',
+  components: {
+    Button,
+  }
+}
+</script>
+
+<style>
+
+</style>
